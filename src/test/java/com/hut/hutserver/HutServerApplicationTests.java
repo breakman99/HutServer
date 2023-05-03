b92fb3247f1309ed61cc00d461156ea7dd7c0e0e
breakman <breakman99@foxmail.com>
2023-05-03 23:12:39 +0800

    init

---
+++ src/test/java/com/hut/hutserver/HutServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hut.hutserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HutServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
