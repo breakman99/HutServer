9d2cbe7695f555a4a77891e658f981d282d34bdc
breakman <breakman99@foxmail.com>
2023-05-13 23:46:32 +0800

    init

--- src/main/java/com/hut/hutserver/entity/request/RegisterRequest.java
+++ src/main/java/com/hut/hutserver/entity/request/RegisterRequest.java
@@ -4,7 +4,13 @@ package com.hut.hutserver.entity.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 
 /**
@@ -15,4 +21,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank(message = "用户名不能为空")
+    private String username;
+
+    @NotBlank(message = "用户ID不能为空")
+    private String userId;
+
+    @NotBlank(message = "密码不能为空")
+    @Size(min = 8, message = "密码长度不能小于8位")
+    private String password;
+
+    private String college;
+
+    @Length(min = 11, max = 11, message = "手机号只能为11位")
+    @Pattern(regexp = "^[1][3,4,5,6,7,8,9][0-9]{9}$", message = "手机号格式有误")
+    private String phoneNum;
+
+    private String bio;
+
+    @URL
+    private String avatarUrl;
+
+    private Byte proFlag;
 }
